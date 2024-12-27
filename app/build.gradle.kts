6cf073a125627b23ea965f161102eef4bc743bfd
Кристина Курзенева <blacktimoshka@mail.ru>
2024-12-27 10:53:49 +0500

    Создание панели инструментов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,9 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
+    val nav_version = "2.8.5"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
